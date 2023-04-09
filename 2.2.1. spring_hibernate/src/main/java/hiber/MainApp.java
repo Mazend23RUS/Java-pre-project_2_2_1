7f8f8fdf7705e93b1ac0b0a3467aeddcf0f301cb
Mazend23 <bboy23@mail.ru>
2023-04-09 09:32:51 +0300

    1-й коммит

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,19 +16,61 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+//      Car alexeyCar = new Car("Mersedes",220);
+//      Car dimonCar = new Car("Audi",100);
+//      Car shurikCar = new Car("BMW",525);
+//      Car igorCar = new Car("Жигули",5);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+//      userService.addCar(alexeyCar);
+//      userService.addCar(dimonCar);
+//      userService.addCar(shurikCar);
+//      userService.addCar(igorCar);
+
+//      User alexey = new User("Alexey", "Semenov", "user1@mail.ru");
+//      User dimon = new User("Dimon", "Kammisarov", "user2@mail.ru");
+//      User shurik = new User("Shurik", "Papin", "user3@mail.ru");
+//      User igor = new User("Igor", "Kiselev", "user4@mail.ru");
+
+      User alexey = new User("Alexey", "Semenov", "user1@mail.ru",new Car("Mersedes",220));
+      User dimon = new User("Dimon", "Kammisarov", "user2@mail.ru",new Car("Audi",100));
+      User shurik = new User("Shurik", "Papin", "user3@mail.ru",new Car("BMW",525));
+      User igor = new User("Igor", "Kiselev", "user4@mail.ru",new Car("Жигули",5));
+      userService.add(alexey);
+      userService.add(dimon);
+      userService.add(shurik);
+      userService.add(igor);
+
+
+
+//      userService.addCar(alexeyCar);
+//      userService.addCar(dimonCar);
+//      userService.addCar(shurikCar);
+//      userService.addCar(igorCar);
+
+//      alexey.setCar(alexeyCar);
+//      dimon.setCar(dimonCar);
+//      shurik.setCar(shurikCar);
+//      igor.setCar(igorCar);
+
+
+//      userService.add(alexey);
+//      userService.add(dimon);
+//      userService.add(shurik);
+//      userService.add(igor);
+
+//      userService.add(new User("Alexey", "Semenov", "user1@mail.ru",new Car("Mersedes",220)));
+//      userService.add(new User("Dimon", "Kammisarov", "user2@mail.ru",new Car("Audi",100)));
+//      userService.add(new User("Shurik", "Papin", "user3@mail.ru",new Car("BMW",525)));
+//      userService.add(new User("Igor", "Kiselev", "user4@mail.ru",new Car("Жигули",5)));
+
+//      List<User> users = userService.listUsers();
+//      for (User user : users) {
+//         System.out.println("Id = "+user.getId());
+//         System.out.println("First Name = "+user.getFirstName());
+//         System.out.println("Last Name = "+user.getLastName());
+//         System.out.println("Email = "+user.getEmail());
+//         System.out.println();
+//      }
 
       context.close();
    }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -1,2 +1,64 @@
-package hiber.model;public class Car {
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+//    @MapsId
+//    @OneToOne
+//    @JoinColumn
+    @OneToOne (mappedBy = "car")
+    @PrimaryKeyJoinColumn
+    private User user;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String model;
+
+    int series;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel(String model) {
+        return model;
+    }
+
+    public void setModel() {
+        this.model = model;
+    }
+
+    public int getSeries(int series) {
+        return series;
+    }
+
+    public void setSeries() {
+        this.series = series;
+    }
+
+    public User getUser(User user){
+        return user;
+    }
+
+
+    public void setUser(){
+        this.user = user;
+    }
+
 }
