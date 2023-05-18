9035400de8aba2b3774c2c8e16dd98c986b61712
Alexey <bboy23@mail.ru>
2023-05-18 21:01:55 +0300

    Убрал лишнее из main

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -32,49 +32,26 @@ public class MainApp {
 //      User shurik = new User("Shurik", "Papin", "user3@mail.ru");
 //      User igor = new User("Igor", "Kiselev", "user4@mail.ru");
 
-      User alexey = new User("Alexey", "Semenov", "user1@mail.ru",new Car("Mersedes",220));
-      User dimon = new User("Dimon", "Kammisarov", "user2@mail.ru",new Car("Audi",100));
-      User shurik = new User("Shurik", "Papin", "user3@mail.ru",new Car("BMW",525));
-      User igor = new User("Igor", "Kiselev", "user4@mail.ru",new Car("Жигули",5));
-      userService.add(alexey);
-      userService.add(dimon);
-      userService.add(shurik);
-      userService.add(igor);
-
-
-
-//      userService.addCar(alexeyCar);
-//      userService.addCar(dimonCar);
-//      userService.addCar(shurikCar);
-//      userService.addCar(igorCar);
-
-//      alexey.setCar(alexeyCar);
-//      dimon.setCar(dimonCar);
-//      shurik.setCar(shurikCar);
-//      igor.setCar(igorCar);
-
-
+//      User alexey = new User("Alexey", "Semenov", "user1@mail.ru",new Car("Mersedes",220));
+//      User dimon = new User("Dimon", "Kammisarov", "user2@mail.ru",new Car("Audi",100));
+//      User shurik = new User("Shurik", "Papin", "user3@mail.ru",new Car("BMW",525));
+//      User igor = new User("Igor", "Kiselev", "user4@mail.ru",new Car("Жигули",5));
 //      userService.add(alexey);
 //      userService.add(dimon);
 //      userService.add(shurik);
 //      userService.add(igor);
 
-//      userService.add(new User("Alexey", "Semenov", "user1@mail.ru",new Car("Mersedes",220)));
-//      userService.add(new User("Dimon", "Kammisarov", "user2@mail.ru",new Car("Audi",100)));
-//      userService.add(new User("Shurik", "Papin", "user3@mail.ru",new Car("BMW",525)));
-//      userService.add(new User("Igor", "Kiselev", "user4@mail.ru",new Car("Жигули",5)));
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Model = " + user.getCar().getModel());
+         System.out.println();
+      }
 
-//      List<User> users = userService.listUsers();
-//      for (User user : users) {
-//         System.out.println("Id = "+user.getId());
-//         System.out.println("First Name = "+user.getFirstName());
-//         System.out.println("Last Name = "+user.getLastName());
-//         System.out.println("Email = "+user.getEmail());
-//         System.out.println("Model = " + user.getCar().getModel());
-//         System.out.println();
-//      }
-
-      List<User> users = userService.listUserByCar(alexeyCar);
+      List<User> users1 = userService.listUserByCar(alexeyCar);
       for (User user : users) {
          System.out.println("\n" + "Достаём собственника по модели машины и по серии \n" +
                  "___________________________");
